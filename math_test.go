7e5ffe6ce17e9ef5f6184dff7849b4270a5217ba
Leandro Zanin <sr.leandrozanin@gmail.com>
2023-12-04 17:14:00 -0300

    feat: teste

--- math_test.go
+++ math_test.go
@@ -9,3 +9,11 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da soma esperado é 30, resultado %d", total)
 	}
 }
+
+func TestSoma2(t *testing.T) {
+	total := Soma(20, 20)
+
+	if total != 20 {
+		t.Errorf("Resultado da soma esperado é 40, resultado %d", total)
+	}
+}
